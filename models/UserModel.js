3d0109ee905012f7ecfa5489752aec178c147db1
Ngô Văn Trung <Trung2601.it@gmail.com>
2023-06-04 16:09:14 +0700

    [update] add method encode & compare by bcryptjs :rocket:

--- models/UserModel.js
+++ models/UserModel.js
@@ -1,6 +1,7 @@
 'use strict'
 
 const mongoose = require('mongoose');
+const bcryptjs = require('bcryptjs')
 
 const Schema = mongoose.Schema;
 
@@ -26,6 +27,35 @@ const userSchema = new Schema({
         ref: 'Deck'
     }]
 })
+
+// ! ENCODE PASSWORD USER
+// * Encode before save user (use pre method)
+userSchema.pre('save', async function (next) {
+    // use normal function, not arrow function, because operator 'this' of arrow function is it.
+    try {
+        // Generate a salt
+        const salt = await bcryptjs.genSalt(10);
+        console.log('Salt ', salt);
+        // Generate a password hash (salt + hash)
+        const passwordHashed = await bcryptjs.hash(this.password, salt);
+        // Re-assign password hashed
+        console.log('Old password', this.password);
+        console.log('Password hashed', passwordHashed);
+        this.password = passwordHashed;
+        next();
+    } catch (err) {
+        next(err);
+    }
+})
+
+
+userSchema.methods.isValidPassword = async function (newPassword) {
+    try {
+        return await bcryptjs.compare(newPassword, this.password);
+    } catch (err) {
+        throw new Error(err);
+    }
+}
 
 const User = mongoose.model('User', userSchema)
 
